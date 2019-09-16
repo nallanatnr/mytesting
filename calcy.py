644c1fa5092b21d1f6b71441c40fcb598afa9147
Tirumala Nallana <tnallana@in.ibm.com>
2019-09-16 07:05:40 +0000

    Initial Commit

---
+++ calcy.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    __Add Function ___
+    return x + y 
+
+def substract (x, y):
+        --Substract Function--
+            return x - y
+            
+        def multiply (x, y):
+            --Multiply Function ---
+                return x * y
+            def divide (x, y)
+            --Divide Function --
+            return x / y
+        
+
